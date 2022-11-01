e8c5f5a260b524cc4d573f195f63b0071f546d42
Egor Dvoretskiy <ergo.dvoretskiy@gmail.com>
2022-11-01 19:12:13 +0300

    Add empty service project.

---
+++ TaskNotifierSolution/TaskNotifierService/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskNotifierService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase ETaskNotifier = new ETaskNotifier();
+
+            ServiceBase.Run(ETaskNotifier);
+        }
+    }
+}
